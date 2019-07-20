abcc0c670dade49a097c27ed08a4a5e4e823b75f
Huw-Man <hu.newman@berkeley.edu>
2019-07-20 18:32:17 -0400

    Updates

---
+++ precache-manifest.dc2df5551767d8d28f051c7dd978699b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55df5dc9dabf219c509ece69aaa01b12",
+    "url": "/Hwebsite/index.html"
+  },
+  {
+    "revision": "68f08584a6bd644017ce",
+    "url": "/Hwebsite/static/css/main.b444731e.chunk.css"
+  },
+  {
+    "revision": "57be6a561e1ff29bdadb",
+    "url": "/Hwebsite/static/js/2.5decf5cd.chunk.js"
+  },
+  {
+    "revision": "68f08584a6bd644017ce",
+    "url": "/Hwebsite/static/js/main.c02855e2.chunk.js"
+  },
+  {
+    "revision": "f098a5d773d67b5e9b1e",
+    "url": "/Hwebsite/static/js/runtime~main.440cc965.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hwebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a9284db985adc526389d46473e73ab2e",
+    "url": "/Hwebsite/static/media/myAvatar.a9284db9.svg"
+  }
+]);
