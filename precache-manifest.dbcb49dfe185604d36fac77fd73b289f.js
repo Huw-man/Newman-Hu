73fcbc18588e82de9c7fe8e53b8a2fcb7acba387
Huw-Man <hu.newman@berkeley.edu>
2019-09-10 23:36:10 -0700

    Updates

---
+++ precache-manifest.dbcb49dfe185604d36fac77fd73b289f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fcbd6d3a91148bb67ac2d260acc337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "688cf01d9ec4cabd9e04",
+    "url": "/static/css/main.feaee0bc.chunk.css"
+  },
+  {
+    "revision": "51a0507e8238cd1299b4",
+    "url": "/static/js/2.7a4f7021.chunk.js"
+  },
+  {
+    "revision": "688cf01d9ec4cabd9e04",
+    "url": "/static/js/main.58bed088.chunk.js"
+  },
+  {
+    "revision": "2570a386b4ac66bba957",
+    "url": "/static/js/runtime~main.71d889ac.js"
+  },
+  {
+    "revision": "e12a0a57098357c90679a117a5ddcc5a",
+    "url": "/static/media/Epic Catan dice thumb.e12a0a57.PNG"
+  },
+  {
+    "revision": "f08c524d147f46398cb54013ee7013ee",
+    "url": "/static/media/LinkedIn_logo_initials.f08c524d.png"
+  },
+  {
+    "revision": "b802740e766b9f9a7844098dcab53270",
+    "url": "/static/media/SICK_logo_resized.b802740e.png"
+  },
+  {
+    "revision": "b411f26781180d2e7ed66d89927eb51e",
+    "url": "/static/media/SLAC_Logo.b411f267.png"
+  },
+  {
+    "revision": "cf2811656f3bb66c221b2890b8ba4f60",
+    "url": "/static/media/footer.cf281165.jpg"
+  },
+  {
+    "revision": "edb9b60a8ec482680bcd28a57734fe81",
+    "url": "/static/media/main_image.edb9b60a.JPG"
+  },
+  {
+    "revision": "a9284db985adc526389d46473e73ab2e",
+    "url": "/static/media/myAvatar.a9284db9.svg"
+  },
+  {
+    "revision": "60ed78ce7f96f0d6d39a39e8bcca02d8",
+    "url": "/static/media/sait_resized.60ed78ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Newman-Hu/precache-manifest.97c75bff1d3ce81a6805682d57f7e1b1.js"
+  "/precache-manifest.dbcb49dfe185604d36fac77fd73b289f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Newman-Hu/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
